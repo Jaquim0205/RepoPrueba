a305a51b61e5902ad9501340e5976d56f0eb287a
Jaquim0205 <emiliojaquim8@gmail.com>
2024-06-26 01:10:54 -0600

    Hola Mundo
    -Se agreo Hola mundo  a la main camera
    -Utilizamos diferentes funciones de debug para imprimir diferentes mensajes

---
+++ Poyecto1Ebac/Assets/Scripts/HolaMundo.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolaMundo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("algo paso");
+        Debug.Log("Hola Mundo");
+        Debug.LogWarning("Algo puede estar mal");
+        Debug.LogError("Algo esta mal");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
